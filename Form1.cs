3daa159459c08bdcf96f0687c6651f2708e56469
samuel law <samuel.law_hk@yahoo.com.hk>
2025-03-10 17:04:01 -0400

    initiate with C# solution
    add C# solution folder

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Book_Store_Stock_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
